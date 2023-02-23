f1ed64a0fd769d8035ddcbc2da565376bd72efa9
raffaez <rafaeleasouzas@gmail.com>
2023-02-23 04:37:00 -0300

    chore: add files array to independent file

---
+++ src/common/files-content.ts
@@ -0,0 +1,21 @@
+
+export const files = [
+  {
+    name: "index.html",
+  },
+  {
+    name: "about.css",
+  },
+  {
+    name: "education.ts",
+  },
+  {
+    name: "skills.json",
+  },
+  {
+    name: "projects.ts",
+  },
+  {
+    name: "contact.tsx",
+  },
+];

b2e3cad6096534ca7d134b9b742108054aa22374
raffaez <rafaeleasouzas@gmail.com>
2023-02-23 02:42:11 -0300

    rearrange: move environment components to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import Sidebar from "./components/Sidebar";
-import Environment from "./components/Environment";
 import "./styles/global.css";
+
+import React from "react";
+
+import BottomBar from "./components/BottomBar";
+import Explorer from "./components/Explorer";
+import Sidebar from "./components/Sidebar";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
-      <Environment />
+    <div className="h-screen w-screen flex flex-col">
+      <TopBar />
+      <div className="flex flex-row flex-grow">
+        <Sidebar />
+        <Explorer />
+        <div>conteúdo</div>
+      </div>
+      <BottomBar />
     </div>
   );
 }

a7fb81cc482cc7a55284315aa0509466105d7094
raffaez <rafaeleasouzas@gmail.com>
2023-02-24 02:50:40 -0300

    fix: set defaultopen to md screen and up

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Explorer from "./Explorer";
 import { Disclosure, Transition } from "@headlessui/react";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function SidePanel() {
+  const isMobile = useMediaQuery("(max-width: 767px)");
+  const defaultOpen = !isMobile;
+
   return (
     <div className="flex">
-      <Disclosure defaultOpen>
+      <Disclosure defaultOpen={defaultOpen}>
         <Sidebar />
         <Transition
           enter="transition-opacity duration-75"

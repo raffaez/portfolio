a2e48bbf55c991687ad9d16f2c6ecf90caa57ff3
raffaez <rafaeleasouzas@gmail.com>
2023-02-24 02:47:36 -0300

    feat: add 'portfolio' directory

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { VscEllipsis, VscFolder, VscJson } from "react-icons/vsc";
-import { HiChevronDown, HiChevronLeft, HiChevronRight } from "react-icons/hi2";
-import { AiFillHtml5 } from "react-icons/ai";
-import { FaReact } from "react-icons/fa";
-import TsIcon from "./icons/TsIcon";
-import { IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
 import { Disclosure, Transition } from "@headlessui/react";
+import React, { useEffect, useState } from "react";
+import { FcFolder, FcOpenedFolder } from "react-icons/fc";
+import { HiChevronRight } from "react-icons/hi2";
+import { VscEllipsis } from "react-icons/vsc";
 import { Link, useNavigate } from "react-router-dom";
-import { ImFileEmpty } from "react-icons/im";
+
 import { files } from "../common/files-content";
 import File from "./File";
 
@@ -25,7 +22,7 @@ function Explorer() {
 
   return (
     <div className="flex flex-col h-full w-64 bg-purple-600 text-purple-300">
-      <div className="flex flex-row justify-between p-3">
+      <div className="flex flex-row justify-between py-2 px-6">
         <div className="uppercase">Explorer</div>
         <VscEllipsis />
       </div>
@@ -38,7 +35,7 @@ function Explorer() {
                   open ? "rotate-90" : "rotate-0"
                 } transform transition duration-200 ease-in-out`}
               />
-              <div className="uppercase font-bold">Portfolio</div>
+              <div className="uppercase font-bold">Rafaele Souza</div>
             </Disclosure.Button>
             <Transition
               appear={true}
@@ -50,19 +47,46 @@ function Explorer() {
               leaveTo="opacity-0"
             >
               <Disclosure.Panel>
-                {files.map((file) => (
-                  <Link
-                    to={`/${file.name.split(".")[0]}`}
-                    key={file.name}
-                    className={`flex pl-7 py-0.5  ${
-                      isActive(file.name)
-                        ? "bg-purple-450 text-white"
-                        : "hover:bg-purple-450 hover:text-purple-200"
-                    }`}
-                  >
-                    <File fileName={file.name} />
-                  </Link>
-                ))}
+                <Disclosure defaultOpen>
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex items-center space-x-1 pl-5 w-full hover:bg-purple-450 hover:text-purple-200">
+                        <HiChevronRight
+                          className={`${
+                            open ? "rotate-90" : "rotate-0"
+                          } transform transition duration-200 ease-in-out`}
+                        />
+                        {open ? <FcOpenedFolder /> : <FcFolder />}
+                        <div className="">portfolio</div>
+                      </Disclosure.Button>
+                      <Transition
+                        appear={true}
+                        enter="transition-opacity duration-75"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity duration-150"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Disclosure.Panel>
+                          {files.map((file) => (
+                            <Link
+                              to={`/${file.name.split(".")[0]}`}
+                              key={file.name}
+                              className={`flex pl-10 py-0.5  ${
+                                isActive(file.name)
+                                  ? "bg-purple-450 text-white"
+                                  : "hover:bg-purple-450 hover:text-purple-200"
+                              }`}
+                            >
+                              <File file={file} />
+                            </Link>
+                          ))}
+                        </Disclosure.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Disclosure>
               </Disclosure.Panel>
             </Transition>
           </>
